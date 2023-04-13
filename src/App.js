ab3cdd4896985d33aa367fe90f59fe9cbea77518
jaswanthakhil <jaswanth.akhil@vervenest.in>
2023-04-13 17:38:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,26 +4,27 @@ import About from './Components/About';
 import Content from './Components/Content';
 import Header from './Components/Header';
 import Contact from './Components/Contact';
-
+import Projects from './Components/Projects';
 import Login from './Components/Login';
-import {Routes, Route, BrowserRouter,Navigate} from 'react-router-dom'
+import { Routes, Route, BrowserRouter, HashRouter as Router, Navigate } from 'react-router-dom'
 import Dashboard from './Components/Dashboard';
 
-export default function App(){
+export default function App() {
   // const location = window.location.pathname
   // console.log(location);
   return (
-    <BrowserRouter>
-         <Header />
-      <Routes>     
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-     
-          <Route path="/login" element={<Login />} />
-          {/* <Route path="/home" element={<Dashboard />} /> */}
-          <Route path="/home" element={<Navigate replace to="/login" />} />
-          <Route index element={<Content />} />
+    <Router>
+      <Header />
+      <Routes>
+   
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/myprojects" element={<Projects />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Dashboard />} />
+        <Route index element={<Content />} />
+   
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
